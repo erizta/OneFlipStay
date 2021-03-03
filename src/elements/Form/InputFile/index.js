e29540c4ca248cd45f85166fb0cc42a24a92aa9a
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-03-03 10:40:31 +0700

    Component input file

--- src/elements/Form/InputFile/index.js
+++ src/elements/Form/InputFile/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import propTypes from 'prop-types'
 import "./index.scss"
 
-export default function Text(props) {
+export default function File(props) {
 
     const {
         name,
@@ -16,31 +16,7 @@ export default function Text(props) {
         errorResponse,
     } = props
 
-    const [HasError, setHasError] = userState(null)
-    let pattern = ""
-
-    if (type === "email") pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if (type === "tel") pattern = "[0-9]*"
-
-    const onChange = (event) => {
-        const target = {
-            target: {
-                name: name,
-                value: event.target.value
-            }
-        }
-    }
-
-    if (type === "email") {
-        if (!pattern.test(event.target.value)) setHasError(errorResponse)
-        else setHasError(null)
-    }
-    if (type === "tel") {
-        if (event.target.validity.valid) props.onChange(target)
-    }
-    else {
-        props.onChange(target)
-    }
+    const refInputFile = useRef(null)
 
 
     return (
@@ -52,14 +28,19 @@ export default function Text(props) {
                     </div>
                 )}
                 <input
+                    accept={accept}
+                    ref={ref}
                     name={name}
-                    type={type}
-                    pattern={pattern}
                     value={value}
-                    readOnly
-                    className={["form-control", inputClassName].join(" ")}
+                    type="file"
+                    className="d-none"
+                    onChange={props.onChange}
+                />
+                <input
+                    onClick={() => refInputFile.current.click()}
+                    defaultValue={value}
                     placeholder={placeholder}
-                    onChange={onChange}
+                    className={["form-control", inputClassName].join(" ")}
                 />
                 {append && (
                     <div className="input-group-append bg-gray-900">
@@ -67,26 +48,21 @@ export default function Text(props) {
                     </div>
                 )}
             </div>
-            {HasError && <span className="error-helper">{HasError}</span>}
-
         </div>
     )
 }
 
-Text.defaultProps = {
-    type: "text",
-    pattern: "",
-    placeholder: "Please type here...",
-    errorResponse: "Please match the requested format.",
+File.defaultProps = {
+    placeholder: "Browse a file..."
 }
 
-Text.propTypes = {
+File.propTypes = {
     name: propTypes.string.isRequired,
-    value: propTypes.oneOfType([propTypes.number, propTypes.string]).isRequired,
+    accept: propTypes.string.isRequired,
+    value: propTypes.string.isRequired,
     onChange: propTypes.func.isRequired,
     prepend: propTypes.oneOfType([propTypes.number, propTypes.string]),
     append: propTypes.oneOfType([propTypes.number, propTypes.string]),
-    type: propTypes.string,
     placeholder: propTypes.string,
     outerClassName: propTypes.string,
     inputClassName: propTypes.string,
