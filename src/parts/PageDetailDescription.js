c05f5c72e563e946fd1162c3579f8ea77eb18249
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-04-13 10:06:09 +0700

    Update PageDetailDescription.js

--- src/parts/PageDetailDescription.js
+++ src/parts/PageDetailDescription.js
@@ -7,26 +7,28 @@ export default function PageDetailDescription({ data }) {
             <h4>About the place</h4>
             {ReactHtmlParser(data.description)}
             <div className="row" style={{ marginTop: 30 }}>
-                {data.fetureId.map((feature, index) => {
-                    return (
-                        <div
-                            className="col-3"
-                            key={`feature-${index}`}
-                            style={{ marginBottom: 20 }}
-                        >
-                            <img
-                                src={feature.imageUrl}
-                                alt={feature.className}
-                                className="d-block mb-2"
-                            />
-                            {" "}
-                            <span>{feature.qty}</span>{" "}
-                            <span className="text-gray-500 font-weight-light">
-                                {feature.name}
-                            </span>
-                        </div>
-                    )
-                })}
+                {
+                    data.featureId.length === 0 ? "Tidak ada Feature" :
+                        data.fetureId.map((feature, index) => {
+                            return (
+                                <div
+                                    className="col-3"
+                                    key={`feature-${index}`}
+                                    style={{ marginBottom: 20 }}
+                                >
+                                    <img
+                                        src={feature.imageUrl}
+                                        alt={feature.className}
+                                        className="d-block mb-2"
+                                    />
+                                    {" "}
+                                    <span>{feature.qty}</span>{" "}
+                                    <span className="text-gray-500 font-weight-light">
+                                        {feature.name}
+                                    </span>
+                                </div>
+                            )
+                        })}
             </div>
         </main>
     )
