0a6554f131fb3137db2042607c6fde80744da655
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-04-13 09:58:54 +0700

    Update DetailsPage.js

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -1,53 +1,60 @@
-import React, { Component } from "react";
-import Fade from "react-reveal/Fade";
-import { connect } from "react-redux";
+import React, { Component } from "react"
+import Fade from "react-reveal/Fade"
+import { connect } from "react-redux"
 
-import Header from "parts/Header";
-import PageDetailTitle from "parts/PageDetailTitle";
-import FeaturedImage from "parts/FeaturedImage";
-import PageDetailDescription from "parts/PageDetailDescription";
-import BookingForm from "parts/BookingForm";
-import Categories from "parts/Categories";
-import Testimony from "parts/Testimony";
-import Footer from "parts/Footer";
+import Header from "parts/Header"
+import PageDetailTitle from "parts/PageDetailTitle"
+import FeaturedImage from "parts/FeaturedImage"
+import PageDetailDescription from "parts/PageDetailDescription"
+import BookingForm from "parts/BookingForm"
+import Categories from "parts/Categories"
+import Testimony from "parts/Testimony"
+import Footer from "parts/Footer"
 
 import ItemDetails from "json/itemDetails.json"
 
-import { checkoutBooking } from "store/actions/checkout";
-// import { fetchPage } from "store/actions/page";
+import { checkoutBooking } from "store/actions/checkout"
+import { fetchPage } from "store/actions/page"
 
 class DetailsPage extends Component {
   componentDidMount() {
     window.title = "Details Page"
     window.scrollTo(0, 0)
-}
+
+    if (!this.props.page[this.props.match.params.id])
+      this.props.fetchPage(`${process.env.REACT_APP_HOST}/api/v1/member/detail-page/${this.props.match.params.id}`, this.props.match.params.id)
+
+  }
 
   render() {
-    const { page, match } = this.props;
 
-    if (!ItemDetails) return null;
+    const { page, match } = this.props
+
+    console.log(page)
+
+    if (!page[match.params.id]) return null
 
     const breadcrumb = [
       { pageTitle: "Home", pageHref: "" },
       { pageTitle: "House Details", pageHref: "" },
-    ];
+    ]
 
     return (
       <>
         <Header {...this.props} />
-        <PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
-        <FeaturedImage data={ItemDetails.imageId} />
+        <PageDetailTitle breadcrumb={breadcrumb} data={page[match.params.id]} />
+        <FeaturedImage data={page[match.params.id].imageId} />
         <section className="container">
           <div className="row">
             <div className="col-7 pr-5">
               <Fade bottom>
-                <PageDetailDescription data={ItemDetails} />
+                <PageDetailDescription data={page[match.params.id]} />
               </Fade>
             </div>
             <div className="col-5">
               <Fade bottom>
                 <BookingForm
-                  itemDetails={ItemDetails}
+                  itemDetails={page[match.params.id]}
                   startBooking={this.props.checkoutBooking}
                 />
               </Fade>
@@ -55,16 +62,19 @@ class DetailsPage extends Component {
           </div>
         </section>
 
-        <Categories data={ItemDetails.categories} />
-        <Testimony data={ItemDetails.testimonial} />
+        <Categories data={page[match.params.id].category} />
+        <Testimony data={page[match.params.id].testimonial} />
 
         <Footer />
       </>
-    );
+    )
   }
 }
 
+const mapStateToProps = (state) => ({
+  page: state.page
+})
 
-export default connect(null, { checkoutBooking })(
+export default connect(mapStateToProps, { checkoutBooking, fetchPage })(
   DetailsPage
-);
+)
